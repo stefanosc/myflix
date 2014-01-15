8d1e246945d5c2bbc3ac656207ea5add0a65a70e
Stefano Schiavi <stefano@bvprojects.org>
2014-01-15 11:50:55 +0100

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+6.times do
+  Video.create(title: "Monk", 
+              description: "This is the best monk movie. Enjoy your membership", 
+              small_cover_url: "public/tmp/monk.jpg", 
+              large_cover_url: "public/tmp/monk_large.jpg")
+
+  Video.create(title: "Family Guy", 
+              description: "This is the best Family Guy movie. Enjoy your membership", 
+              small_cover_url: "public/tmp/family_guy.jpg", 
+              large_cover_url: "public/tmp/monk_large.jpg")
+
+end
