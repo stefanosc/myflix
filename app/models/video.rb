76242ac59e9b474588577d0e8758b7f0d1a7859a
Stefano Schiavi <stefano@bvprojects.org>
2014-02-03 20:01:30 +0100

    added search results categorized

--- app/models/video.rb
+++ app/models/video.rb
@@ -13,4 +13,18 @@ class Video < ActiveRecord::Base
     where('title LIKE ?', "%#{search_term}%").order("created_at DESC")
   end
 
+  def self.search_by_title_categorized(search_term)
+    raw_results = self.search_by_title(search_term)
+    raw_results.reduce({}) do |collection, video|
+      if collection[video.category].nil?
+        collection[video.category] = []
+        collection[video.category] << video
+      else
+        collection[video.category] << video        
+      end
+      collection
+    end
+  end
+
+
 end
