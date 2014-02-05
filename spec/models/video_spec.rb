5156937ec1229df4d305babeb0c82537604d8643
Stefano Schiavi <stefano@bvprojects.org>
2014-02-05 22:39:35 +0100

    added spec for search_by_title_categorized

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -8,7 +8,7 @@ describe Video do
   it { should validate_presence_of(:title)}
   it { should validate_presence_of(:category)}
 
-  describe "::search_by_title" do
+  describe ".search_by_title" do
     it "searches by title and returns empty array if not found" do
       c = Category.create(name: "monk category", description: "category description")
       v = Video.create(title: "this is a Monk movie", description: "this is the monk description", category: c )
@@ -39,6 +39,22 @@ describe Video do
       v1 = Video.create(title: "this is a Future movie", description: "this is the future description", category: c )
       expect(Video.search_by_title('movie')).to eq([v1, v])         
     end
+  end
+
+  describe ".search_by_title_categorized" do
+    it "returns search results in a hash categorized" do
+      c1 = Category.create(name: "future", description: "category description")
+      c2 = Category.create(name: "adventure", description: "category description")
+      c3 = Category.create(name: "love", description: "category description")
+      v6 = Video.create(title: "futurama friends", description: "this is the monk description", category: c1, created_at: 1.day.ago )
+      v5 = Video.create(title: "almost human friends", description: "this is the monk description", category: c1, created_at: 2.day.ago )
+      v4 = Video.create(title: "friends adventure", description: "this is the monk description", category: c2, created_at: 3.day.ago )
+      v3 = Video.create(title: "trekking friends", description: "this is the monk description", category: c2, created_at: 4.day.ago )
+      v2 = Video.create(title: "love friends", description: "this is the monk description", category: c3, created_at: 5.day.ago )
+      v1 = Video.create(title: "romance friends", description: "this is the future description", category: c3 )
+      expect(Video.search_by_title_categorized("friends")).to eq({c1 => [v6,v5], c2 => [v4,v3], c3 => [v1,v2]})
+    end
 
   end
+
 end
