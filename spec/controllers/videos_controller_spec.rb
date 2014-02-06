a9c07743295f0f72d04ddeee67f92356f6ab2288
Stefano Schiavi <stefano@bvprojects.org>
2014-02-07 00:46:51 +0100

    video controller spec - video and category fabricator

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe VideosController do
+
+  subject { Fabricate{:video} }
+
+  describe "GET #show" do
+    it "assigns the requested video to @video" do
+      get :show, id: 1
+      expect(subject).to == video
+    end
+    it "renders the :show template" do
+
+    end
+  end
+
+  describe "GET #search" do
+    it "assigns the search term to @q" do
+      
+    end
+    it "it assigns the result as an array of arrays in blocks of 6 to @videos" do
+      
+    end
+    it "renders the :search template" do
+      
+    end
+  end
+
+end
---
+++ spec/fabricators/video_fabricator.rb
@@ -0,0 +1,7 @@
+Fabricator{:video} do
+  
+  title { Faker::Lorem.word + "movie" }
+  description { Faker::Lorem.sentence(15) }
+  category
+
+end
