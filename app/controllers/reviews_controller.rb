f79b50e20d8280c8cf3f72bd744d8761a6688713
Stefano Schiavi <stefano@bvprojects.org>
2014-02-15 22:40:03 +0100

    extracted review_params to private method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,18 @@ class ReviewsController < ApplicationController
 
   def create
     @video = Video.find_by(slug: params[:video_id])
-    @review = @video.reviews.build(params.require(:review).permit(:body, :rating).merge!({user: current_user}))
+    @review = @video.reviews.build(review_params.merge!({user: current_user}))
     if @review.save
       redirect_to @video
     else
       @reviews = @video.reviews.reload
       render 'videos/show'
     end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body, :rating)
   end
 end 
