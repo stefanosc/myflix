3d03a18d6cbd3b8eb91c8cae9e8694def7ebde88
Stefano Schiavi <stefano@bvprojects.org>
2014-02-15 21:22:00 +0100

    added @reviews and @video examples

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -3,63 +3,59 @@ require 'spec_helper'
 describe ReviewsController do
 
   describe "POST #create" do
-    context 'with authenticated user' do
-      context 'with valid input' do
 
-        let(:current_user) { Fabricate(:user) }
-        before {session[:user] = current_user.id }
+    let(:video) { video = Fabricate(:video) }
+
+    context 'with authenticated user' do
+
+      let(:current_user) { Fabricate(:user) }
+      before {session[:user] = current_user.id }
+
+      context 'with valid input' do
+        before { post :create, review: Fabricate.attributes_for(:review), video_id: video.slug }
 
         it "redirects to the video page" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.slug
           expect(response).to redirect_to video_path(video)
         end
         it "saves the review to the db" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.slug
           expect(Review.count).to eq(1)
         end
         it "saves a review associated with the video" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.slug
           expect(Review.first.video).to eq(video)
         end
         it "saves a review associated with the current user" do
-          video = Fabricate(:video)
-          post :create, review: Fabricate.attributes_for(:review), video_id: video.slug
           expect(Review.first.user).to eq(current_user)
         end
-        # it "sets a success flash notice" do
-        #   video = Fabricate(:video)
-        #   post :create, review: Fabricate.attributes_for(:review), video_id: video.slug
-        # end
       end
 
       context 'with in-valid input' do
-        let(:current_user) { Fabricate(:user) }
-        before {session[:user] = current_user.id }
 
         it "does not save the review to the db" do
-          video = Fabricate(:video)
           post :create, review: {rating: 5}, video_id: video.slug
           expect(Review.count).to eq(0)
         end
-        it "renders the video page" do
-          video = Fabricate(:video)
+        it "renders the video/show template" do
           post :create, review: {rating: 5}, video_id: video.slug
-          expect(response).to render_template 'videos/show' 
+          expect(response).to render_template 'videos/show'
         end
-        it "displays an error message" do
-
+        it "sets the @video variable" do
+          review = Fabricate(:review, video: video)
+          post :create, review: {rating: 5}, video_id: video.slug
+          expect(assigns(:video)).to eq(video)
+        end
+        it "sets the @reviews variable" do
+          review = Fabricate(:review, video: video)
+          post :create, review: {rating: 5}, video_id: video.slug
+          expect(assigns(:reviews)).to match_array([review])
         end
       end
     end
 
     context 'with un-authenticated user' do
+
       it "redirects to the sing_in_path" do
-        video = Fabricate(:video)
         post :create, review: {rating: 5}, video_id: video.slug
-        expect(response).to redirect_to sign_in_path 
+        expect(response).to redirect_to sign_in_path
       end
     end
   end
