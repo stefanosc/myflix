f1a290156ec02f094b95e131d497ba760beec103
Stefano Schiavi <stefano@bvprojects.org>
2014-02-23 06:01:55 +0100

    added validation spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Category do
 
   it { should have_many(:videos)}
+  it { should validate_presence_of(:name) }
 
   describe "#recent_videos" do
     it "returns all if less than 6 videos mathcing" do
