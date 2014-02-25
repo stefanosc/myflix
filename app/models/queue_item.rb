33fd0a62fa7bded2acf63422a424a360432fcbf8
Stefano Schiavi <stefano@bvprojects.org>
2014-02-25 16:31:57 +0100

    validation syntax fixes

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -2,8 +2,8 @@ class QueueItem < ActiveRecord::Base
   belongs_to :video
   belongs_to :user
 
-  validates_presence_of :user, :video
-  validates_uniqueness_of :video, scope: :user
+  validates_presence_of :user_id, :video_id
+  validates_uniqueness_of :video_id, scope: :user_id
 
   delegate :category, to: :video
   delegate :title, to: :video, prefix: :video
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe QueueItem do
 
-  it { should belong_to(:video) } 
-  it { should belong_to(:user) } 
-  it { should validate_presence_of(:user) }
-  it { should validate_presence_of(:video) }
+  it { should belong_to(:video_id) } 
+  it { should belong_to(:user_id) } 
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:video_id) }
 
   describe "#video_title" do
     it "returns the title of the video associated" do
