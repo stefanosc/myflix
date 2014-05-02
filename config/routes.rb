52c1b9687f3c99d168b5d0754ddfc8e7bae76ee2
Stefano Schiavi <stefano@bvprojects.org>
2014-05-03 00:46:45 +0200

    user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
-
-  root to: 'pages#front'
-  get 'home', to: 'videos#index'
-
-  get 'register', to: 'users#new'
-
-  get 'sign-out', to: 'sessions#destroy',  as: 'sign_out'
-  get 'sign-in', to: 'sessions#new', as: 'sign_in'
-  post 'sign-in', to: 'sessions#create'
-
-  resources :users, only: [:create, :edit, :update]
-
-  resources :queue_items, path: 'my-queue' , only: [:index, :create, :destroy]
-  post 'update-queue', to: 'queue_items#update_queue', as: 'update_queue'
 
   resources :videos, except:[:destroy, :index] do
     collection do
@@ -21,9 +6,23 @@ Myflix::Application.routes.draw do
     end
     resources :reviews, only:[:create]
   end
-
   post 'remove-review', to: 'reviews#destroy', as: 'destroy'
 
   resources :categories, only: [:index, :show]
+
+  resources :queue_items, path: 'my-queue' , only: [:index, :create, :destroy]
+  post 'update-queue', to: 'queue_items#update_queue', as: 'update_queue'
+
+  get 'register', to: 'users#new'
+  resources :users, only: [:create, :edit, :update, :show]
+
+  get 'sign-out', to: 'sessions#destroy',  as: 'sign_out'
+  get 'sign-in', to: 'sessions#new', as: 'sign_in'
+  post 'sign-in', to: 'sessions#create'
+
+  root to: 'pages#front'
+  get 'home', to: 'videos#index'
+
+  get 'ui(/:action)', controller: 'ui'
 
 end
