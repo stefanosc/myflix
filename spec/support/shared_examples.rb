0aa255722fac24c4d260be8486de1d9c65083d76
Stefano Schiavi <stefano@bvprojects.org>
2014-08-10 22:49:21 +0200

    added 'requires admin' example

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -6,6 +6,15 @@ shared_examples "requires user to sign in" do
   end
 end
 
+shared_examples "requires user to be admin" do
+  it "redirects to root_path when signed in user is not admin" do
+    clear_current_user
+    set_current_user
+    action
+    expect(response).to redirect_to root_path
+  end
+end
+
 shared_examples "redirects to invalid token page with invalid token" do
   it "renders the token invalid page" do
     action
