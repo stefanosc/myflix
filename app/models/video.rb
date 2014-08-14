5ac83ae25634806f34453572399ff0deb4a12d7e
Stefano Schiavi <stefano@bvprojects.org>
2014-08-14 23:57:22 +0200

    mounted uploader for small and large cover

--- app/models/video.rb
+++ app/models/video.rb
@@ -10,6 +10,9 @@ class Video < ActiveRecord::Base
 
   validates_presence_of :title, :description, :category
 
+  mount_uploader :large_cover, LargeCoverUploader
+  mount_uploader :small_cover, SmallCoverUploader
+
   def self.search_by_title(search_term)
     return [] if search_term == "" || search_term == nil
     where('title LIKE ?', "%#{search_term}%").order("created_at DESC")
