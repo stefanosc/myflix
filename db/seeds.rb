0e1869e5323899554dc53c63b417f51475f22baa
Stefano Schiavi <stefano@bvprojects.org>
2014-08-18 13:29:23 +0200

    updated db seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.delete_all
-Video.delete_all
-User.delete_all
-Review.delete_all
+video_path = [ "http://loveflix.s3.amazonaws.com/video/HW2%20Solution.mp4",
+               "http://loveflix.s3.amazonaws.com/video/HW2%20set%20up%20unicorn%20and%20sidekiq%20on%20production.mp4"
+             ]
 
 %w(Action Sci-Fi Fantasy).each do |category|
   description = "In this category you will find the most beautiful #{category.downcase} movies"
-  Category.create(name: category, description: description)
+  p Category.create(name: category, description: description)
 end
 
-Dir.glob('public/tmp/*.jpg').sort.each do |image|
-  unless image.match("large")
-    movie_title = File.basename(image,'.jpg').gsub('_', ' ').titleize
-    movie_thumbnail = image.gsub('public', '')
-  end
+Dir.glob('public/images/video/small_cover/*.jpg').sort.each do |image|
+  p movie_title = File.basename(image,'.jpg').gsub('_', ' ').titleize
+  p movie_thumbnail = File.basename(image)
   categories = Category.all
   video = Video.create(title: movie_title,
               description: "#{movie_title} is just awesome. Enjoy your membership",
-              small_cover: movie_thumbnail,
-              large_cover: "/tmp/monk_large.jpg",
-              category_id: categories[rand(3)].id )
+              category_id: categories[rand(3)].id,
+              video_url: video_path.sample
+              )
+  p video.update_columns(small_cover: movie_thumbnail, large_cover: "monk_large.jpg")
 end
 
 users = []
 users << mark = User.create(name: "Mark Jacob", email: "mark@example.com", password: "mark")
 users << kelly = User.create(name: "Kelly Jones", email: "kelly@example", password: "kelly")
-users << stefano = User.create(name: "Stefano Schiavi", email: "stefano@bvprojects.org", password: "stefano")
+users << stefano = User.create(name: "Stefano Schiavi", email: "stefano@bvprojects.org", password: "stefano", admin: true)
 
 Video.all.each do |video|
   review = ["I quite like this movie", "This movie was fantastic", "It was ok", "Best movie ever"]
