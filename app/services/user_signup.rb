b12c63e67e76d0965b1041301e2025cd03445126
Stefano Schiavi <stefano@bvprojects.org>
2015-04-19 19:53:37 +0200

    cleanup UserSignup service class

--- app/services/user_signup.rb
+++ app/services/user_signup.rb
@@ -1,31 +1,12 @@
 class UserSignup
 
-  attr_reader :user, :charge_token, :invite_token, :error_message
+  attr_reader :user, :error_message
 
   def initialize(user, charge_token, invite_token)
     @user = user
-    @charge_token = charge_token
-    @invite_token = invite_token
     @status = nil
     @error_message = nil
-    signup!
-  end
-
-  def signup!
-    if user.valid?
-      charge = charge_payment
-      if charge.successful?
-        user.save
-        handle_invitation
-        AppMailer.delay.welcome_email(@user)
-        @status = :success
-      else
-        @error_message = charge.error_message
-        @status = :error
-      end
-    else
-      @status = :error
-    end
+    signup(charge_token, invite_token)
   end
 
   def successful?
@@ -38,14 +19,31 @@ class UserSignup
 
   private
 
-  def handle_invitation
-    if invite = Invite.find_by(token: invite_token)
+  def signup(charge_token, invite_token)
+    if user.valid?
+      charge = charge_payment(charge_token)
+      if charge.successful?
+        user.save
+        handle_invitation(invite_token)
+        AppMailer.delay.welcome_email(@user)
+        @status = :success
+      else
+        @error_message = charge.error_message
+        @status = :error
+      end
+    else
+      @status = :error
+    end
+  end
+
+  def handle_invitation(invite_token)
+    if invite = Invite.where(token: invite_token).first
       @user.followers << invite.inviter
       invite.inviter.followers << @user
     end
   end
 
-  def charge_payment
+  def charge_payment(charge_token)
     StripeWrapper::Charge.create(
       :amount => 999,
       :card => charge_token
