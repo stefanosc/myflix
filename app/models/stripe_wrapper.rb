95c1ae41d8ccb42b7156163b2fa31b5df2d02514
Stefano Schiavi <stefano@bvprojects.org>
2015-04-25 01:33:51 +0200

    add StripeWrapper::Customer for subscription plans
    - added StripeWrapper::Customer class
    - added specs
    - added VCR cassette

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -30,4 +30,36 @@ module StripeWrapper
     end
 
   end
+
+  class Customer
+
+    attr_reader :response, :status
+
+    def initialize(response, status)
+      @response = response
+      @status   = status
+    end
+
+    def self.create(source:, email:, plan: "basic-plan")
+      begin
+        response = Stripe::Customer.create(
+                     source: source,
+                     plan:   plan,
+                     email:  email
+                   )
+        new(response, :success)
+      rescue Stripe::CardError => e
+        new(e, :error)
+      end
+    end
+
+    def successful?
+      status == :success
+    end
+
+    def error_message
+      response.message
+    end
+  end
 end
+
